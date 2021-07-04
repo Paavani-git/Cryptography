781b5da64dbe743fd8cefb6c10b43fb8fd29d4f5
Pavani Poluru <76960304+Paavani-git@users.noreply.github.com>
2021-07-05 01:31:44 +0530

    Update 5.py

--- cryptopals/set1/5.py
+++ cryptopals/set1/5.py
@@ -1,9 +1,5 @@
-from itertools import cycle
-from binascii import hexlify
-from codecs import *
-def rep_xor(text,key):
-	x = ("".join(chr(ord(i)^ord(j)) for i,j in zip(text,cycle(key)))
-return (x.encode("utf-8").hex()) 
-msg=b"Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"""
-key = b"ICE"
-print(rep_xor(msg,key))
+def rep_xor(text,key): 
+    return b''.join([bytes([key[i%len(key)]^j]) for i,j in enumerate(text)]) 
+msg = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal" 
+key = b"ICE" 
+print(rep_xor(msg, key).hex()) 
